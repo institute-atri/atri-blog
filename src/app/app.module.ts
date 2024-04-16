a3e827ee1294f89dd9e41da0e89c1fc6a2b45bae
AllanPeterson <al.peterson@outlook.com>
2024-04-16 18:55:51 -0300

    feat: add login and register screens (#1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MatSidenavContainer, MatSidenavModule } from '@angular/material/sidenav';
-import { SidenavComponent } from './components/home/sidenav/sidenav.component';
-import { ContentHeaderComponent } from './components/home/content-header/content-header.component';
-import { ContentComponent } from './components/home/content/content.component';
-import { FooterComponent } from './components/home/footer/footer.component';
-import { PagenavComponent } from './components/home/pagenav/pagenav.component';
-import { LoginComponent } from './login/login.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ContentHeaderComponent } from './components/content-header/content-header.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PagenavComponent } from './components/pagenav/pagenav.component';
+import { LoginComponent } from './screens/login/login.component';
+import { RegisterComponent } from './screens/register/register.component';
+import { HomeComponent } from './screens/home/home.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { LoginComponent } from './login/login.component';
     ContentComponent,
     FooterComponent,
     PagenavComponent,
-    LoginComponent
+    LoginComponent, 
+    RegisterComponent, 
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
