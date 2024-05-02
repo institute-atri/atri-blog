3b0d05f66a41693f13709868bfad18f47c171a4d
AllanPeterson <al.peterson@outlook.com>
2024-05-02 20:19:27 -0300

    feat: add settings screen (#3)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './screens/login/login.component';
 import { HomeComponent } from './screens/home/home.component';
 import { RegisterComponent } from './screens/register/register.component';
+import { SettingsComponent } from './screens/settings/settings.component';
 
 const routes: Routes = [
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent }, 
+  { path: 'settings', component: SettingsComponent },
   
 ];
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { SidenavComponent } from './components/home/sidenav/sidenav.component';
-import { ContentHeaderComponent } from './components/home/content-header/content-header.component'; 
-import { ContentComponent } from './components/home/content/content.component';
-import { PagenavComponent } from './components/home/pagenav/pagenav.component';
-import { FooterComponent } from './components/home/footer/footer.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ContentHeaderComponent } from './components/content-header/content-header.component'; 
+import { ContentComponent } from './components/content/content.component';
+import { PagenavComponent } from './components/pagenav/pagenav.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { MatSidenavModule } from '@angular/material/sidenav'; // Importe apenas o MatSidenavModule
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
